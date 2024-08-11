6aa78b729b3a68317b1c47b04c99ed89e35d95b5
kennedymwaniki <kennedymwaniki089@gmail.com>
2024-08-11 06:28:37 +0300

    img fix

--- app/_components/CabinCard.js
+++ app/_components/CabinCard.js
@@ -7,15 +7,17 @@ function CabinCard({ cabin }) {
 
   return (
     <div className="flex border-primary-800 border">
-      <Image
-        src={image}
-        alt={`Cabin ${name}`}
-        blurDataURL={image}
-        quality={80}
-        width={200}
-        height={200}
-        className="flex-1 border-r border-primary-800 object-cover object-top"
-      />
+      <div className="flex-1 relative">
+        <Image
+          src={image}
+          fill
+          alt={`Cabin ${name}`}
+          blurDataURL={image}
+          quality={80}
+          placeholder="blur"
+          className=" border-r border-primary-800 object-cover object-top"
+        />
+      </div>
 
       <div className="flex-grow">
         <div className="pt-5 pb-4 px-7 bg-primary-950">
