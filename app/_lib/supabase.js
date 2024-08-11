4464ac12f959365aa2a3489b792a1a091dd1217e
kennedymwaniki <kennedymwaniki089@gmail.com>
2024-08-11 06:30:33 +0300

    supabase

--- app/_lib/supabase.js
+++ app/_lib/supabase.js
@@ -2,6 +2,6 @@ import { createClient } from "@supabase/supabase-js";
 
 //! the SUPABASE_KEY allows us to bypass row level security and perfrom all operations because our api is only allowed to authenticated users , but the client requires all operations
 export const supabase = createClient(
-  process.env.SUPABASEUrl,
+  process.env.supabaseUrl,
   process.env.SUPABASE_KEY
 );
