e38ede8a3f75c42b9e06d40733f8bc98d7c0110c
kennedymwaniki <kennedymwaniki089@gmail.com>
2024-08-13 15:53:50 +0300

    filtering

--- app/cabins/page.js
+++ app/cabins/page.js
@@ -1,15 +1,17 @@
 import { Suspense } from "react";
 import CabinList from "../_components/CabinList";
 import Spinner from "../_components/Spinner";
+import Filter from "../_components/Filter";
 
 // //!opting out of the data cache , which will cancel full route cache(the data will always be revalidated)
 // //! revalidation time is in seconds-- 15 id seconds 1hr -> 3600
-export const revalidate = 15;
+//! export const revalidate = 15; cant apply becasue params cant be known at build time, hence it switches to dynamic
 export const metadata = {
   title: "Cabins",
 };
 
-export default function Page() {
+export default function Page({ searchParams }) {
+  const filter = searchParams?.capacity ?? "all";
   return (
     <div>
       <h1 className="text-4xl mb-5 text-accent-400 font-medium">
@@ -23,8 +25,12 @@ export default function Page() {
         home away from home. The perfect spot for a peaceful, calm vacation.
         Welcome to paradise.
       </p>
+      <div className="flex justify-end mb-8">
+        <Filter />
+      </div>
+
       <Suspense fallback={<Spinner />}>
-        <CabinList />
+        <CabinList filter={filter} />
       </Suspense>
     </div>
   );
